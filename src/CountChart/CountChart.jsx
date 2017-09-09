b8a93f41fe47c8454b1d1052a932b4823815b5fd
Ernie Yang <ernieyang09@hotmail.com>
2017-09-09 18:26:56 +0800

    xAxis change

--- src/CountChart/CountChart.jsx
+++ src/CountChart/CountChart.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   LineChart,
   Line,
@@ -8,17 +8,42 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
-} from 'recharts';
-import './style.scss';
+  Surface,
+} from 'recharts'
+import moment from 'moment'
+import * as R from 'ramda'
+import './style.scss'
 
 class CountChart extends Component {
+
+  xTickShow = () => (
+    R.pipe(
+       R.addIndex(R.map)((data, i)=> (this.checkIsMonday(moment(data.Date))) ? i : false),
+       R.reject((data) => !data)
+    )(this.props.dataSet)
+  )
+
+  xAxisShow = (index) => {
+    const dateString = this.props.dataSet[index].Date;
+    return moment(dateString).format('MM/DD');
+  }
+
+  checkIsMonday = (date) => date.format("dddd") === 'Sunday';
+
   render() {
+    const xTick = this.xTickShow()
+
     return (
       <ResponsiveContainer aspect={3}>
         <LineChart data={this.props.dataSet}>
-          <XAxis dataKey='date'/>
+          <XAxis
+            tickFormatter={this.xAxisShow}
+            ticks={xTick}
+          />
           <YAxis/>
-          <CartesianGrid strokeDasharray="3 3"/>
+          <CartesianGrid
+            strokeDasharray="3 3"
+          />
           <Line type="monotone" dataKey="count" stroke="#8884d8" activeDot={{r: 8}}/>
         </LineChart>
       </ResponsiveContainer>
