b01f1ec5025192e4ad7ad2430bb531110139f776
Ernie Yang <ernieyang09@hotmail.com>
2017-09-09 18:07:21 +0800

    change ramda method

--- src/ChartContainer1/ChartContainer1.jsx
+++ src/ChartContainer1/ChartContainer1.jsx
@@ -14,7 +14,7 @@ class ChartContainer extends Component {
     }), Array(31));
     const total = R.addIndex(R.map)( (data, i) => ({
       Date: (i<9) ? `2017-08-0${i+1}` : `2017-08-${i+1}`,
-      count: R.reduce((acc, key) => acc + data[key].count , 0, Object.keys(data))
+      count: R.pipe(R.keys, R.reduce((acc, key) => acc + data[key].count , 0))(data),
     }), detail);
     this.props.UpdateData({total, detail});
   }
